b16a5ce30ac2b6565d2fc7483c0841a16d63810c
hfxz <xuan__zai@outlook.com>
2025-06-26 18:59:04 +0800

    change db schema

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -8,6 +8,7 @@ export async function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      db: { schema: "sale_gain" },
       cookies: {
         getAll() {
           return cookieStore.getAll();
@@ -15,7 +16,7 @@ export async function createClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
+              cookieStore.set(name, value, options)
             );
           } catch {
             // The `setAll` method was called from a Server Component.
@@ -24,6 +25,6 @@ export async function createClient() {
           }
         },
       },
-    },
+    }
   );
 }
