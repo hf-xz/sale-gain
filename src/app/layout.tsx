0f9a0c2a1a677df6b64d74c8ccd13bb269899068
hfxz <xuan__zai@outlook.com>
2025-06-28 19:45:18 +0800

    remove tutorial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Sale Gain",
 };
 
 const geistSans = Geist({
@@ -25,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
