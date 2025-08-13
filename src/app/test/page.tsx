84ad9bce11f4a38d4130a63f6603611559bb8a3d
hfxz <xuan__zai@outlook.com>
2025-08-13 23:33:02 +0800

    去掉 test

--- src/app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getStores } from "@/services/storeService";
-
-export default async function Page() {
-  const stores = await getStores();
-
-  return <pre>{JSON.stringify(stores)}</pre>;
-}
